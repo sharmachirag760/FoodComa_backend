040310ad50a0d6d73b7bb5c8046dfa1a2374bb74
Chirag Sharma <chirag@Chirags-MacBook-Air.local>
2024-12-16 20:51:40 +0530

    user schema tested

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const server = require('./config/ServerConfig')
 const connectDB = require('./config/dbConfig')
+const user = require('./schema/userSchema')
 const app = express();
 app.use(express.json());
 app.use(express.text());
@@ -12,4 +13,13 @@ app.post('/ping',(req,res)=>{
 app.listen(server.PORT, async ()=>{
     await connectDB();
     console.log(`Port started at ${server.PORT} !!`)
+    const newUser = await user.create({
+        firstName : "abc",
+        lastName : "sharma",
+        password : "123456",
+        email : "abcd@gmail.com",
+        mobileNumber : "1234567892"
+    })
+    console.log("created new user")
+    console.log(newUser)
 }) 
