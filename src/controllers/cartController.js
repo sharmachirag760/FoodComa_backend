e6a2a018a497adc92b8f9b8f1392551d6ba6d8b4
Chirag Sharma <chirag@Chirags-MacBook-Air.local>
2024-12-19 17:16:13 +0530

    modify cart function added

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,4 +1,4 @@
-const { getCart } = require("../services/cartService");
+const { getCart, modifyCart } = require("../services/cartService");
 const AppError = require("../utils/appError");
 
 async function getCartByUser(req,res){
@@ -28,6 +28,36 @@ async function getCartByUser(req,res){
         })
     }
 }
+
+async function modifyProductToCart(req,res){
+    try {
+        const cart = await modifyCart(req.user.id,req.params.productId,req.params.operation == 'add');
+        return res.status(200).json({
+            success : true,
+            message : "successfully added product to the cart",
+            error : {},
+            data : cart
+        })
+    } catch (error) {
+        console.log(error);
+        if(error instanceof AppError){
+            return res.status(error.statuscode).json({
+                success : false,
+                message : error.message,
+                error : error,
+                data : {}
+            })
+        }
+        return res.status(500).json({
+            success : false,
+            message : "something went wrong",
+            error : error,
+            data : {}
+        })
+    }
+}
+
 module.exports = {
-    getCartByUser
+    getCartByUser,
+    modifyProductToCart
 }
--- src/services/cartService.js
+++ src/services/cartService.js
@@ -1,5 +1,8 @@
 const { getCartByUserId } = require("../repositories/cartRepository");
+const AppError = require("../utils/appError");
+const BadRequestError = require("../utils/badRequest");
 const notFoundError = require("../utils/notFoundError");
+const { getProductById } = require("./productServices");
 
 async function getCart(userId){
     const cart = await getCartByUserId(userId);
@@ -8,6 +11,57 @@ async function getCart(userId){
     }
     return cart;
 }
+async function modifyCart(userId, productId,shouldAdd = true){
+    const quantityValue = (shouldAdd == true) ? 1 : -1
+    const cart = await getCart(userId);
+    const product = await getProductById(productId);
+    if(!product){
+        throw new notFoundError("product");
+    }
+    if(!product.inStock && product.quantity<=0){
+        throw new BadRequestError(['product not available in stock'])
+    }
+    let foundProduct = false;
+    cart.items.forEach(item =>{
+        if(item.product._id == productId){
+            if(shouldAdd){
+                if(product.quantity >= item.quantity+1) item.quantity += quantityValue;
+                else{
+                    throw new AppError("The quantity for the requested item is not available",404)
+                }
+            }
+            else{
+                if(item.quantity > 0 ){
+                    item.quantity += quantityValue;
+                    if(item.quantity ==0){
+                        cart.items = cart.items.filter(item => item.product._id != productId);
+                        foundProduct = true;
+                        return ;
+                    }
+                }
+                else{
+                    throw new AppError("The quantity for the requested item is not available",404)
+                }
+            }
+            
+       
+            foundProduct = true;
+        }
+    })
+    if(!foundProduct){
+        if(shouldAdd){
+            cart.items.push({
+                product : productId,
+                quantity : 1
+            })
+        }else {
+            throw new notFoundError("product in the cart")
+        }  
+    }
+    await cart.save()
+    return cart;
+}
 module.exports = {
-    getCart
+    getCart,
+    modifyCart
 }
